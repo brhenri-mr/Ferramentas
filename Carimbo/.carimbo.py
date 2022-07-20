63623dda4f27b276ea83464fcb3d93702529e5b6
brhenri-mr <83376956+brhenri-mr@users.noreply.github.com>
2022-07-20 17:45:12 -0300

    Update .carimbo.py

--- Carimbo/.carimbo.py
+++ Carimbo/.carimbo.py
@@ -15,39 +15,47 @@ def login():
                 [sg.Button('Ok'), sg.Button('Cancel')] ]
     return sg.Window('login',layout, finalize = True)
 
-janela = login()
-window,event, values = sg.read_all_windows()
+#janela = login()
+#window,event, values = sg.read_all_windows()
 while True:
+    janela = login()
+    window,event, values = sg.read_all_windows()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
+    
     elif janela == window and event =="Ok":
-        matricial = values[0]
-        destination = values[1]
-        if len(values[2]) >= 2:
-            if values[2][0] =="0":
-                versao = values[2][1]
+        try:
+            matricial = values[0]
+            destination = values[1]
+            if len(values[2]) >= 2:
+                if values[2][0] =="0":
+                    versao = values[2][1]
+                else:
+                    versao = values[2]
             else:
                 versao = values[2]
-        else:
-            versao = values[2]
-        break
+            os.chdir(matricial)
 
-os.chdir(matricial)
+            documentos = os.listdir()
 
-documentos = os.listdir()
+            for documento in documentos:
+                #mudar de diretorio
+                if os.path.isdir(documento):
+                    os.chdir(documento)
+                    local = os.getcwd()
+                    for arquivo in os.listdir():
+                        if (versao+".pdf") in arquivo or (versao+"A.pdf") in arquivo or (versao+").pdf") in arquivo:
+                            try:
+                                shutil.copyfile(src = os.path.join(local,arquivo), dst = os.path.join(destination,arquivo))
+                                print(f"arquivo{arquivo} copiado com sucesso")
+                                break
+                            except:
+                                print(f"tivemos erro no arquivo {arquivo}")
+                    os.chdir(matricial)
+        except:
+            print("insira parametros")
+            widown.close()
 
-for documento in documentos:
-    #mudar de diretorio
-    if os.path.isdir(documento):
-        os.chdir(documento)
-        local = os.getcwd()
-        for arquivo in os.listdir():
-            if (versao+".pdf") in arquivo or (versao+"A.pdf") in arquivo:
-                try:
-                    shutil.copyfile(src = os.path.join(local,arquivo), dst = os.path.join(destination,arquivo))
-                    print(f"arquivo{arquivo} copiado com sucesso")
-                    break
-                except:
-                    print(f"tivemos erro no arquivo {arquivo}")
-        os.chdir(matricial)
+
+
 
